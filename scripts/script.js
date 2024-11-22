e3ac664ebb37182146da6a8ae92420a93fec51ab
J4BC97 <hammerban123@gmail.com>
2024-11-22 17:23:22 -0500

    Replica Pagina Eticket - Actualizacion

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@ const focusableElements = document.querySelectorAll(".menu-boton");
 // Agregar un event listener a cada elemento
 focusableElements.forEach((element) => {
     element.addEventListener("click", () => {
-        // Quitar la clase "focused" de todos los elementos
+        // Quitar la clase "desaparecer" de todos los elementos
         focusableElements.forEach((el) => el.classList.remove("desaparecer"));
-        // Agregar la clase "focused" al elemento actual
+        // Agregar la clase "desaparecer" al elemento actual
         element.classList.add("desaparecer");
     });
 });
 
-// Cerrar el borde al hacer clic fuera de cualquier elemento focusable
+// Cerrar el borde al hacer clic fuera de cualquier elemento menu-boton
 document.addEventListener("click", (event) => {
     if (![...focusableElements].some((el) => el.contains(event.target))) {
         focusableElements.forEach((el) => el.classList.remove("desaparecer"));
